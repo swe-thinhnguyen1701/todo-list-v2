60eb78413cae2b0d3ebcf09996db9aaec063f518
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-01-11 16:13:17 -0800

    BUILD: complete task button

---
+++ src/components/MarkComplete.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@chakra-ui/react";
+
+const MarkComplete = () => {
+    return (
+        <Button tabIndex={-1}>
+            Complete
+        </Button>
+    )
+}
+
+export default MarkComplete;
