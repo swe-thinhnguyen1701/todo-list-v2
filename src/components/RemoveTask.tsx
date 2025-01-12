f9d069d260d6b16eaa671cb9f19384095a50673f
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-01-11 16:12:00 -0800

    BUILD: remove task component

---
+++ src/components/RemoveTask.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@chakra-ui/react";
+
+const RemoveTask = () => {
+    return (
+        <Button tabIndex={-1}>
+            Remove
+        </Button>
+    )
+}
+
+export default RemoveTask;
