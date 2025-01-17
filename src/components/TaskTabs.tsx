9bfe82d917e4d2ac4ea2689b7e62c2062ecd4f0d
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-01-17 14:58:52 -0800

    UPDATE: heading and margin

--- src/components/TaskTabs.tsx
+++ src/components/TaskTabs.tsx
@@ -1,11 +1,11 @@
-import { Tab, TabList, Tabs } from "@chakra-ui/react";
+import { Heading, Tab, TabList, Tabs } from "@chakra-ui/react";
 import { BsCheckSquareFill, BsFillGrid3X3GapFill } from "react-icons/bs";
 
 interface Props {
     onSelectTab: (tab: number) => void;
 }
 
-const TaskTabs = ({onSelectTab}: Props) => {
+const TaskTabs = ({ onSelectTab }: Props) => {
     const TABS = [
         {
             icon: <BsFillGrid3X3GapFill />,
@@ -18,17 +18,20 @@ const TaskTabs = ({onSelectTab}: Props) => {
     ];
 
     return (
-        <Tabs variant="unstyled" onChange={(index) => onSelectTab(index)}>
-            <TabList flexDir={{base: "row", md: "column"}} gap={{base: "0", md: "10px"}}>
+        <>
+            <Heading as="h2" fontSize={{base: "2xl"}} textAlign="center" marginBottom="10px">Task Status Overview</Heading>
+            <Tabs variant="unstyled" onChange={(index) => onSelectTab(index)} marginBottom={{base: "20px", md: "0"}}>
+                <TabList flexDir={{ base: "row", md: "column" }} gap={{ base: "0", md: "10px" }}>
                     {TABS.map((tab, index) => {
                         return (
-                            <Tab _selected={{color: "white", fontWeight: "bold", bg: "blue.500"}} key={index} width="100%">
+                            <Tab _selected={{ color: "white", fontWeight: "bold", bg: "blue.500" }} key={index} width="100%">
                                 {tab.lable}
                             </Tab>
                         )
                     })}
-            </TabList>
-        </Tabs>
+                </TabList>
+            </Tabs>
+        </>
     )
 }
 
