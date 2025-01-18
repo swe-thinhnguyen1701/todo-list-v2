58c2bc3d402babe4f439b6f811ccd3ee73cf92c8
Charles Nguyen <swe.thinhnguyen@gmail.com>
2025-01-17 16:02:03 -0800

    UPDATE: minor update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,34 @@ import NavBar from './components/NavBar'
 import './App.css'
 import TaskList from './components/TaskList'
 import { Grid, GridItem } from '@chakra-ui/react'
+import TaskTabs from './components/TaskTabs'
+import { useState } from 'react'
+import TaskForm from './components/TaskForm'
+import useTasks from './hooks/useTasks'
 
 function App() {
-
+  const [selectedTab, setSelectTab] = useState(0);
+  const tasks = useTasks(selectedTab);
 
   return (
     <>
-      <Grid templateAreas={`"nav" "main"`}
-            padding={5}
-            gap={10}>
+      <Grid templateAreas={{
+        base: `"nav" "aside" "main"`,
+        md: `"nav nav" "aside main"`
+      }}
+        templateColumns={{
+          base: "1fr",
+          md: "200px 1fr"
+        }}>
         <GridItem area="nav">
           <NavBar />
         </GridItem>
+        <GridItem area="aside">
+          <TaskTabs onSelectTab={setSelectTab}/>
+        </GridItem>
         <GridItem area="main">
-        <TaskList />
+          <TaskForm isInProgressTab={selectedTab === 0} />
+          <TaskList tasks={tasks}/>
         </GridItem>
       </Grid>
     </>
